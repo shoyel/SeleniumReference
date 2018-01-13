51e25869ac0b5a9a844fc127e8640d2a19ff8c21
Iftekhar Ivaan <iivaan@shiftedtech.com>
2018-01-13 11:55:50 -0500

    Keyword driven script

---
+++ src/test/java/com/shiftedtech/qa/scripts/spreekeyword/SpreeDriver.java
@@ -0,0 +1,48 @@
+package com.shiftedtech.qa.scripts.spreekeyword;
+
+import com.shiftedtech.qa.spree.SpreeScriptBaseTestNg;
+import com.shiftedtech.qa.spree.utils.ExcelReader;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by ShiftTeacher on 1/13/2018.
+ */
+public class SpreeDriver extends SpreeScriptBaseTestNg {
+
+
+
+    @Test
+    public void driverScript(){
+        Object[][] data = null;
+        String dataFile = System.getProperty("user.dir") + "/src/test/resources/KW-Scripts/KWScript3.xlsx";
+
+        ExcelReader reader = new ExcelReader(dataFile);
+        data = reader.getExcelSheetData("Login02",true);
+        for(int i = 0; i < data.length; i++){
+           System.out.println("Row[" + i + "] " + Arrays.deepToString(data[i]));
+
+               String stepNumber = data[i][0].toString();
+               String keyword = data[i][1].toString();
+               String keywordData = data[i][2].toString();
+               System.out.println("Step[" + stepNumber + "] " + keyword);
+
+               if(keyword.equalsIgnoreCase("verifyPageTitle")) {
+                   homePage.verifyPageTitle(keywordData);
+               }else if(keyword.equalsIgnoreCase("navigateToLoginPage")) {
+                   homePage.navigateToLoginPage();
+               }else if(keyword.equalsIgnoreCase("login")) {
+                   String dataPart[] = keywordData.split("\\|");
+                   loginPage.login(dataPart[0], dataPart[1]);
+               }else if(keyword.equalsIgnoreCase("verifyLoginSuccess")) {
+                   homePage.verifyLoginSuccess();
+               }else if(keyword.equalsIgnoreCase("verifyLoginNotSuccess")) {
+                   loginPage.verifyLoginNotSuccess();
+               }else{
+                   System.out.println("******** Unknown KEY WORD *************");
+               }
+        }
+    }
+}
---
+++ src/test/java/com/shiftedtech/qa/scripts/spreekeyword/SpreeDriverEx2.java
@@ -0,0 +1,64 @@
+package com.shiftedtech.qa.scripts.spreekeyword;
+
+import com.shiftedtech.qa.spree.SpreeScriptBaseTestNg;
+import com.shiftedtech.qa.spree.utils.ExcelReader;
+import org.testng.SkipException;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+/**
+ * Created by ShiftTeacher on 1/13/2018.
+ */
+public class SpreeDriverEx2 extends SpreeScriptBaseTestNg {
+
+    @Test(dataProvider = "scriptNames")
+    public void driverScript(String scriptFile, String scriptName, String runScript){
+        Object[][] data = null;
+        String dataFile = System.getProperty("user.dir") + scriptFile;
+
+        if(runScript.equalsIgnoreCase("N")){
+            throw new SkipException("Script[" + scriptName + "] " + "skipped");
+        }
+
+        ExcelReader reader = new ExcelReader(dataFile);
+        data = reader.getExcelSheetData(scriptName,true);
+        for(int i = 0; i < data.length; i++){
+            System.out.println("Row[" + i + "] " + Arrays.deepToString(data[i]));
+
+            String stepNumber = data[i][0].toString();
+            String keyword = data[i][1].toString();
+            String keywordData = data[i][2].toString();
+            System.out.println("Step[" + stepNumber + "] " + keyword);
+
+            if(keyword.equalsIgnoreCase("verifyPageTitle")) {
+                homePage.verifyPageTitle(keywordData);
+            }else if(keyword.equalsIgnoreCase("navigateToLoginPage")) {
+                homePage.navigateToLoginPage();
+            }else if(keyword.equalsIgnoreCase("login")) {
+                String dataPart[] = keywordData.split("\\|");
+                loginPage.login(dataPart[0], dataPart[1]);
+            }else if(keyword.equalsIgnoreCase("verifyLoginSuccess")) {
+                homePage.verifyLoginSuccess();
+            }else if(keyword.equalsIgnoreCase("verifyLoginNotSuccess")) {
+                loginPage.verifyLoginNotSuccess();
+            }else{
+                System.out.println("******** Unknown KEY WORD *************");
+            }
+        }
+    }
+
+    @DataProvider
+    public static Object[][] scriptNames(){
+        Object[][] data = {
+                           {"/src/test/resources/KW-Scripts/KWScript1.xlsx","Login01","Y"},
+                           {"/src/test/resources/KW-Scripts/KWScript2.xlsx","Login02","Y"},
+                           {"/src/test/resources/KW-Scripts/KWScript3.xlsx","Login01","Y"},
+                           {"/src/test/resources/KW-Scripts/KWScript3.xlsx","Login02","Y"},
+                           {"/src/test/resources/KW-Scripts/KWScript3.xlsx","Login03","Y"},
+                           {"/src/test/resources/KW-Scripts/KWScript3.xlsx","Login04","Y"}
+                          };
+        return data;
+    }
+}
